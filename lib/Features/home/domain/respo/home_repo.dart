974941364d4216a7e5a583092b5250b752c00236
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2024-05-23 20:21:44 +0300

    added either library to manage the repos

--- lib/Features/home/domain/respo/home_repo.dart
+++ lib/Features/home/domain/respo/home_repo.dart
@@ -1,10 +1,18 @@
-// the respo class should be abstract class 
+// the respo class should be abstract class
 // inside this class: we used to wirte what will happend in the app NOT how to will happn
 import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly_app/core/error/failure.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  
-   Future<List<BookEntity>> fetchFeatureBooks(); 
+  // any one user this method, requred to do two thing one at the right side and
+  // other one at the left side : one if there was a problem will be at the left side
+  // and other one if there is NO a proble and will be at the right side
+  // if there no problem will return List of Book entities
+  // but if there is a problem will return => i create a class for error in the core
+  // and will return the error of there is a problem occur in this function
 
-   Future<List<BookEntity>> fetchNewsestBooks();
- }
+  Future<Either<Faliure, List<BookEntity>>> fetchFeatureBooks();
+
+  Future<Either<Faliure, List<BookEntity>>> fetchNewsestBooks();
+}
