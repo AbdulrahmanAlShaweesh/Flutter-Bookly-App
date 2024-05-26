dd6a05bf1157e1e6ab8088bf24a15203825e6324
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2024-05-26 11:21:36 +0300

    refactor use case class

--- lib/Features/home/domain/use_case/fetch_featured_book_use_case.dart
+++ lib/Features/home/domain/use_case/fetch_featured_book_use_case.dart
@@ -3,14 +3,22 @@ import 'package:bookly_app/Features/home/domain/respo/home_repo.dart';
 import 'package:bookly_app/core/error/failure.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchFeaturedBooksUseCase {
+class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
   final HomeRepo homeRepo;
 
   FetchFeaturedBooksUseCase(this.homeRepo);
-
-  Future<Either<Faliure, List<BookEntity>>> fetchFeatureBooks(){
-    return homeRepo.fetchFeatureBooks();
-    
+  @override
+  Future<Either<Faliure, List<BookEntity>>> call(NoParam? param) async {
+    return await homeRepo.fetchFeatureBooks();
   }
 }
 
+// generic class: once we use the class,it will ask us to use tyle that will return once the
+// data come
+abstract class UseCase<Tyle, Param> {
+  // passe optional parameter, or we can use any type
+  Future<Either<Faliure, Tyle>> call(Param param);
+}
+
+// we use this class we do not need to use parameter.
+class NoParam {}
