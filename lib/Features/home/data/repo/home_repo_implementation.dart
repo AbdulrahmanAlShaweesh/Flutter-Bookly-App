36ae433fe51f55455df16d41e1d338fc2ddd1730
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2024-05-28 15:16:33 +0300

    create server filaure from dio exption

---
+++ lib/Features/home/data/repo/home_repo_implementation.dart
@@ -0,0 +1,57 @@
+import 'package:bookly_app/Features/home/data/data_sources/home_local_data_source.dart';
+import 'package:bookly_app/Features/home/data/data_sources/home_remote_data_source.dart';
+import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly_app/Features/home/domain/respo/home_repo.dart';
+import 'package:bookly_app/core/error/failure.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+class HomeRepoImplementation extends HomeRepo {
+  // In this class we need to implement the two methods, through remote and local data sources
+  // so, the two sources "local, remote" will be used here
+  // the benifit of using artichture is to sprate the domain layer from data
+  // so, with using his artichture, any change in the domain layer will not effect the data layer.
+  // so, by using this class we will be able to connect between both local and remote data sources.
+  final HomeRemoteDataSource homeremotedatasource;
+  final HomeLocalDataSource homelocaldatasource;
+
+  HomeRepoImplementation(
+      {required this.homeremotedatasource, required this.homelocaldatasource});
+  @override
+  Future<Either<Faliure, List<BookEntity>>> fetchFeatureBooks() async {
+    try {
+      List<BookEntity> books;
+      books = homelocaldatasource.fetchFeatureBooks();
+      if (books.isNotEmpty) {
+        //return the data in the right side which is the books incase there is no error or issues
+        return right(books);
+      }
+      books = await homeremotedatasource.fetchFeatureBooks();
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFaliure.fromDioError(e));
+      }
+      return left(ServerFaliure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Faliure, List<BookEntity>>> fetchNewsestBooks() async {
+    try {
+      List<BookEntity> books;
+      books = homelocaldatasource.fetchFeatureBooks();
+      if (books.isNotEmpty) {
+        //return the data in the right side which is the books incase there is no error or issues
+        return right(books);
+      }
+      books = await homeremotedatasource.fetchFeatureBooks();
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFaliure.fromDioError(e));
+      }
+      return left(ServerFaliure(e.toString()));
+    }
+  }
+}
--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -1,4 +1,46 @@
-
-class Faliure {
-  
+import 'package:dio/dio.dart';
+abstract class Faliure {
+  final String message;
+  Faliure(this.message);
+}
+class ServerFaliure extends Faliure {
+  ServerFaliure(super.message);
+  factory ServerFaliure.fromDioError(DioException e) {
+    switch (e.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFaliure('Connection time out with api server');
+      case DioExceptionType.sendTimeout:
+        return ServerFaliure('Send time out with api server');
+      case DioExceptionType.receiveTimeout:
+        return ServerFaliure('Recived time out with api server');
+      case DioExceptionType.badCertificate:
+        return ServerFaliure('badCertificate with api server');
+      case DioExceptionType.cancel:
+        return ServerFaliure('Request to api server was canceld');
+      case DioExceptionType.connectionError:
+        return ServerFaliure('No Internet connection');
+      case DioExceptionType.unknown:
+        return ServerFaliure('Opps there was an error, please try again later');
+      case DioExceptionType.badResponse:
+        return ServerFaliure.fromResponse(
+            e.response!.statusCode, e.response!.data);
+    }
+  }
+  factory ServerFaliure.fromResponse(int? statusCode, dynamic response) {
+    // we need to look at the api we are working with and get the errors e.g 404 error etx ...
+    if (statusCode == 404) {
+      return ServerFaliure(
+          'Your request was not found, please try again later');
+    } else if (statusCode == 500) {
+      return ServerFaliure(
+        'There is a problem with the server, please try again later',
+      );
+    } else if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFaliure(
+        response['error']['message'],
+      );
+    } else {
+      return ServerFaliure('There was an error, please try again');
+    }
+  }
 }
